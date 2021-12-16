34b5895dd37f142c58f151a7b03d76ac57ea9d0f
BUAAhuangsh <70243994+BUAAhuangsh@users.noreply.github.com>
2021-12-16 20:48:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,29 @@
 import re,sys
-# reserved_words_dict={'int':'define',
-#                      'main':'dso_local i32 @main',
-#                      '(':'(',
-#                      ')':')',
-#                      'return':'ret',
-#                      '{':'{',
-#                      '}':'}',
-#                      ';':'',
-#                      '/':'/',
-#                      '*':'*'}
-# # 运算符表
-# y_list = {"+","-","*","/","<","<=",">",">=","=","==","!=","^",",","&","&&","|","||","%","~","<<",">>","!"}
-# # 分隔符表
-# f_list = {";","(",")","[","]","{","}", ".",":","\"","#","\'","\\","?"}
-# # 关键字表
-# k_list = {
-#     "auto", "break", "case", "char", "const", "continue","default", "do", "double", "else", "enum", "extern",
-#     "float", "for", "goto", "if", "int", "long","register", "return", "short", "signed", "sizeof", "static",
-#     "struct", "switch", "typedef", "union", "unsigned", "void","volatile", "while", "printf"
-# }
-# Cmp = ["<", ">", "==", "!=", "<=", ">="]
-# Type = {"int","float","char","double","void","long","unsigned","string"}
-# type_flag = ""
-# # 括号配对判断
-# kuo_cp = {'{':'}', '[':']', '(':')'}
+reserved_words_dict={'int':'define',
+                     'main':'dso_local i32 @main',
+                     '(':'(',
+                     ')':')',
+                     'return':'ret',
+                     '{':'{',
+                     '}':'}',
+                     ';':'',
+                     '/':'/',
+                     '*':'*'}
+# 运算符表
+y_list = {"+","-","*","/","<","<=",">",">=","=","==","!=","^",",","&","&&","|","||","%","~","<<",">>","!"}
+# 分隔符表
+f_list = {";","(",")","[","]","{","}", ".",":","\"","#","\'","\\","?"}
+# 关键字表
+k_list = {
+    "auto", "break", "case", "char", "const", "continue","default", "do", "double", "else", "enum", "extern",
+    "float", "for", "goto", "if", "int", "long","register", "return", "short", "signed", "sizeof", "static",
+    "struct", "switch", "typedef", "union", "unsigned", "void","volatile", "while", "printf"
+}
+Cmp = ["<", ">", "==", "!=", "<=", ">="]
+Type = {"int","float","char","double","void","long","unsigned","string"}
+type_flag = ""
+# 括号配对判断
+kuo_cp = {'{':'}', '[':']', '(':')'}
 # # 正则表达式判断是否为数字
 # def if_num(int_word):
 #     int_word = number_parsing(int_word)
@@ -59,48 +59,48 @@ import re,sys
 # # 返回值为列表out_words
 # # 列表元素{'word':ws, 'line':line_num}分别对应单词与所在行号
 #
-# def get_word():
-#     global f_list
-#     out_words = []
-#     # 先逐行读取，并记录行号
-#     lines = []
-#     for line in sys.stdin:
-#         lines.append(line)
-#     line_num = 1
-#     # 判断是否含有注释块的标识
-#     pass_block = False
-#     for line in lines:
-#         words = list(line.split())
-#         for w in words:
-#             # 去除注释
-#             if '*/' in w:
-#                 pass_block = False
-#                 continue
-#             if '//' in w:
-#                 break
-#             if '/*' in w:
-#                 pass_block = True
-#                 continue
-#             if pass_block:
-#                 continue
-#             # 分析单词
-#             if w in Cmp:
-#                 out_words.append({'word':w, 'line':line_num})
-#                 continue
-#             ws = w
-#             for a in w:
-#                 if a in f_list or a in y_list:
-#                     # index为分隔符的位置，将被分隔符或运算符隔开的单词提取
-#                     index = ws.find(a)
-#                     if index!=0:
-#                         # 存储单词与该单词的所在行号，方便报错定位
-#                         out_words.append({'word':ws[0:index], 'line':line_num})
-#                     ws = ws[index+1:]
-#                     out_words.append({'word':a, 'line':line_num})
-#             if ws!='':
-#                 out_words.append({'word':ws, 'line':line_num})
-#         line_num += 1
-#     return out_words
+def get_word():
+    global f_list
+    out_words = []
+    # 先逐行读取，并记录行号
+    lines = []
+    for line in sys.stdin:
+        lines.append(line)
+    line_num = 1
+    # 判断是否含有注释块的标识
+    pass_block = False
+    for line in lines:
+        words = list(line.split())
+        for w in words:
+            # 去除注释
+            if '*/' in w:
+                pass_block = False
+                continue
+            if '//' in w:
+                break
+            if '/*' in w:
+                pass_block = True
+                continue
+            if pass_block:
+                continue
+            # 分析单词
+            if w in Cmp:
+                out_words.append({'word':w, 'line':line_num})
+                continue
+            ws = w
+            for a in w:
+                if a in f_list or a in y_list:
+                    # index为分隔符的位置，将被分隔符或运算符隔开的单词提取
+                    index = ws.find(a)
+                    if index!=0:
+                        # 存储单词与该单词的所在行号，方便报错定位
+                        out_words.append({'word':ws[0:index], 'line':line_num})
+                    ws = ws[index+1:]
+                    out_words.append({'word':a, 'line':line_num})
+            if ws!='':
+                out_words.append({'word':ws, 'line':line_num})
+        line_num += 1
+    return out_words
 #
 # class word_list():
 #     def __init__(self,in_words):
@@ -233,5 +233,7 @@ def main():
     lines=[]
     for line in sys.stdin:
         lines.append(line)
-    print(lines)
+    for i in lines:
+        print(i)
+
 main()
